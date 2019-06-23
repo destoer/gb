ec2c1d3588d527fab1c3431a3ee0960bfbce1cf2
destoer <regulusbootleggers@hotmail.com>
2019-06-23 19:12:27 +0100

    Delete build.sh

--- build.sh
+++
@@ -1,4 +0,0 @@
-clang -g -DSOUND  -O3  -Wextra -Wshadow  -Wall -Wpointer-arith -Wformat=2 -Wcast-qual src/rom.c src/memory.c src/debug.c src/apu.c src/opcode.c src/opcode_cb.c src/instr.c src/banking.c src/lib.c src/main.c src/cpu.c src/disass.c  src/ppu.c src/joypad.c -lSDL2main -lSDL2 -o emu
-#-fsanitize=address
-#-Wshadow
-#-Wconversion
