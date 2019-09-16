af44b8c5b58abfed05bb02f599849f06a8168777
destoer <regulusbootleggers@hotmail.com>
2019-09-16 14:11:19 +0100

    Delete build.bat

--- build.bat
+++
@@ -1,2 +0,0 @@
-gcc  -g -O3   -Wextra  -Wall -DDEBUG -DSOUND -DCGB src/apu.c src/gb.c src/memory.c src/rom.c src/debug.c src/opcode.c src/opcode_cb.c src/instr.c src/banking.c src/lib.c src/main.c src/cpu.c src/disass.c  src/ppu.c src/joypad.c -lmingw32 -lSDL2main -lSDL2 -o emu
-
